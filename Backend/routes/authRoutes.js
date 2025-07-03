d45f2ca2e3b3c6213acf2d98114bf62b204451f5
hello-world123pratik <pratikshirsath45@gmail.com>
2025-07-03 19:18:40 +0530

    Update authRoutes.js

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,30 +1,9 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// Register
-router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) return res.status(400).json({ message: "Fields required" });
-  const exists = await User.findOne({ email });
-  if (exists) return res.status(400).json({ message: "User exists" });
-  const hash = await bcrypt.hash(password, 10);
-  const u = await User.create({ name, email, password: hash });
-  res.status(201).json({ message: "Registered" });
-});
-
-// Login
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user || !(await bcrypt.compare(password, user.password)))
-    return res.status(400).json({ message: "Invalid creds" });
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-  res.json({ message: "Logged in", token });
-});
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
