99443ec693fb79a411bc30a1fe0dc94477e88903
hello-world123pratik <pratikshirsath45@gmail.com>
2025-07-11 18:50:42 +0530

    Update uploadMiddleware.js

--- Backend/middleware/uploadMiddleware.js
+++ Backend/middleware/uploadMiddleware.js
@@ -22,6 +22,6 @@ const storage = multer.diskStorage({
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 50 * 1024 * 1024, // 🔥 50MB limit
+    fileSize: 50 * 1024 * 1024, 
   },
 });
